55e7f0de21e599bff70cd04771b5fa8eb73521ca
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-19 13:02:11 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,5 +8,5 @@
 # Initialize a repository with LineageOS
 repo init --depth 1 -u https://github.com/LineageOS/android -b cm-14.1
 mkdir .repo/local_manifests
-cp android_local_manifest_jiayu/local_manifests.xml .repo/local_manifests
+cp -f android_local_manifest_jiayu/local_manifests.xml .repo/local_manifests
 repo forall -vc "git reset --hard"
