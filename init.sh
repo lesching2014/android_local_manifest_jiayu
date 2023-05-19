dcb43e13a9f4185acdfe37ad3cef2c9c8e262805
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-19 13:10:30 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@
 repo init --depth 1 -u https://github.com/LineageOS/android -b cm-14.1
 mkdir .repo/local_manifests
 cp -f android_local_manifest_jiayu/local_manifests.xml .repo/local_manifests
-#repo forall -vc "git reset --hard"
+# repo forall -vc "git reset --hard"
