996d4c4079a828d45bf23eca2489c9a61a57bee1
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-19 12:11:34 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# build ROM:
+# export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8g"
+# export     ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx3G"
+# ./prebuilts/sdk/tools/jack-admin kill-server
+# ./prebuilts/sdk/tools/jack-admin start-server
+
+echo "Building ROM"
+echo "1. boot image only"
+echo "2. recovery image only"
+echo "3. system image only"
+echo "4. all image (boot + recovery + system)"
+read -p "Do you want to sync NOW? [Y,n]" -i Y input
+
+export LC_ALL=C
+source build/envsetup.sh
+breakfast s3_h560
+
+case $input in  
+  1) make bootimage ;; 
+  2) make recoveryimage ;; 
+  3) make systemimage ;;
+  4) brunch s3_h560 ;;  
+  *) echo dont know ;; 
+esac
