03ab397cf6320be0abe97831d118061a224dc175
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-19 13:37:42 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,8 +4,7 @@ echo "Repo sync method"
 echo "n. network only"
 echo "l. local only"
 echo "f. full sync"
-echo "Choose your option:"
-read input
+read -p "Choose your option:" input
 
 case $input in  
   n|N) repo sync -n ;; 
