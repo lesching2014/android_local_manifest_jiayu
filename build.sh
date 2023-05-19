d50766b65c87af518895b1da1f5ca4a06f27a141
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-19 21:42:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@
 # ./prebuilts/sdk/tools/jack-admin kill-server
 # ./prebuilts/sdk/tools/jack-admin start-server
 
+export JAVA_HOME=/usr/lib/jvm/java-9-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
 echo "Building ROM"
 echo "1. boot image only"
 echo "2. recovery image only"
