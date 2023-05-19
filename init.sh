de7c5cacfd70c572d214a37d139f629da3d0e496
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-19 12:50:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # git clone https://github.com/lesching2014/android_local_manifest_jiayu
+# cp -f ./android_local_manifest_jiayu/init.sh init.sh
+# cp -f ./android_local_manifest_jiayu/prebuilt.sh prebuilt.sh
+# cp -f ./android_local_manifest_jiayu/build.sh build.sh
 
 # Initialize a repository with LineageOS
 repo init --depth 1 -u https://github.com/LineageOS/android -b cm-14.1
