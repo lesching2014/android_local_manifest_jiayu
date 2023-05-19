808aa0640b87c3633d43f22b02eac08c856384c4
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-19 12:52:38 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,3 @@ repo init --depth 1 -u https://github.com/LineageOS/android -b cm-14.1
 mkdir .repo/local_manifests
 cp android_local_manifest_jiayu/local_manifests.xml .repo/local_manifests
 repo forall -vc "git reset --hard"
-read -p "Do you want to sync NOW? [Y,n]" -i Y input
-if [[ $input == "Y" || $input == "y" ]]; then
-    repo sync 
-fi
