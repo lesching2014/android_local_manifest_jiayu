d56e9a857fa176d5ec4e84ab2e2a069430495047
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-20 09:07:09 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,11 +4,11 @@ echo "Repo sync method"
 echo "n. network only"
 echo "l. local only"
 echo "f. full sync"
-read -p "Choose your option:" input
+read -p "Choose your option:(Default:F)" input
 
 case $input in  
   n|N) repo sync -n ;; 
   l|L) repo sync -l ;; 
-  f|F) repo sync ;;
+  f|F|"") repo sync ;;
   *) echo dont know ;; 
 esac
