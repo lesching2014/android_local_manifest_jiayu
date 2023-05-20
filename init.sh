89687258aba9f75f88f17afe645b27600cef17bd
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-20 09:02:03 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Initialize a repository with LineageOS
-repo init --depth 1 -u https://github.com/LineageOS/android -b cm-14.1
+repo init --depth=1 --manifest-url=https://github.com/LineageOS/android -b cm-14.1
 mkdir -p .repo/local_manifests
 cp -f android_local_manifest_jiayu/local_manifests.xml .repo/local_manifests
 # repo forall -vc "git reset --hard"
