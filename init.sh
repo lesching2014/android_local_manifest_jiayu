1fcf13858ad6933f919fede2fdfea56cc276b5dd
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-29 11:21:51 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+mkdir -p ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+FILE=~/.profile
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    echo PATH="$HOME/bin:$PATH" >> $FILE
+fi
+source $FILE
+
 # Initialize a repository with LineageOS
 repo init --depth=1 --manifest-url=https://github.com/LineageOS/android -b cm-14.1
 mkdir -p .repo/local_manifests
