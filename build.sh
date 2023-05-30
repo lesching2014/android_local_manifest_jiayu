fc929ca85d22c9b3e90eabe47898bd0f7f51935a
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-30 13:12:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # export PATH=$JAVA_HOME/bin:$PATH
 
 # Increase Java maximum memory to 4G
-read -p "Do you want to increase to 4G? [Y,n]" -i Y IncreaseAns
+read -p "Do you want to increase Java memory to 4G? [Y,n]" -i Y IncreaseAns
 
 # Build ROM
 echo "Building ROM"
@@ -15,14 +15,14 @@ echo "3. system image only"
 echo "4. all image (boot + recovery + system)"
 read -p "Choose your option:[Enter key:4]" input
 
+source build/envsetup.sh
+breakfast s3_h560
 if [[ $IncreaseAns == "Y" || $IncreaseAns == "y" || $IncreaseAns == "" ]]; then
     export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8g"
     # ./prebuilts/sdk/tools/jack-admin kill-server
     # ./prebuilts/sdk/tools/jack-admin start-server
 fi
 export LC_ALL=C
-source build/envsetup.sh
-breakfast s3_h560
 
 case $input in  
   1) make -j4 bootimage ;; 
