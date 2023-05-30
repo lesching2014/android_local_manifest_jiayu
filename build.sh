13eb5ab2339aca20758376188845cff666a86c0a
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-05-30 13:10:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Increase Java maximum memory to 4G
-read -p "Do you want to increase to 4G? [Y,n]" -i Y input
-if [[ $input == "Y" || $input == "y" || $input == "" ]]; then
-    export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8g"
-    # ./prebuilts/sdk/tools/jack-admin kill-server
-    # ./prebuilts/sdk/tools/jack-admin start-server
-fi
-
 # export     ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx3G"
 # export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 # export PATH=$JAVA_HOME/bin:$PATH
+
+# Increase Java maximum memory to 4G
+read -p "Do you want to increase to 4G? [Y,n]" -i Y IncreaseAns
 
 # Build ROM
 echo "Building ROM"
@@ -20,6 +15,11 @@ echo "3. system image only"
 echo "4. all image (boot + recovery + system)"
 read -p "Choose your option:[Enter key:4]" input
 
+if [[ $IncreaseAns == "Y" || $IncreaseAns == "y" || $IncreaseAns == "" ]]; then
+    export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8g"
+    # ./prebuilts/sdk/tools/jack-admin kill-server
+    # ./prebuilts/sdk/tools/jack-admin start-server
+fi
 export LC_ALL=C
 source build/envsetup.sh
 breakfast s3_h560
