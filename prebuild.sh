4ab0467b414ebfd5c1bcb9a70ea1700185cca8dc
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-06-11 15:30:01 +0800

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -5,10 +5,11 @@ read -p "Do you want to install esseential packages? [Y,n]" -i Y input
 if [[ $input == "Y" || $input == "y" || $input == "" ]]; then
     sudo apt install bc bison build-essential ccache curl \
     flex g++-multilib gcc-multilib git git-lfs gnupg gperf \
-    imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev \
+    imagemagick lib32ncurses-dev lib32readline-dev lib32z1-dev \
     libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev \
     libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool \
-    squashfs-tools xsltproc zip zlib1g-dev libwxgtk3.0-dev openjdk-8-jdk python2.7 python3
+    squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python2.7
+    sudo apt install libwxgtk3.0-dev python3
 fi
 
 # Create ccache
