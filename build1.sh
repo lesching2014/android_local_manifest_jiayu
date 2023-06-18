b79920ba04c91c4939e7deadbb7b1bfa2fa0f862
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-06-18 10:16:06 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -2,9 +2,10 @@
 #for d in ./*/ ; do (cd "$d" && git lfs pull); done
 size="10G"
 file_swap=/swapfile_$size.img
-if [ -e "$file_swap" ]; then
-    sudo touch $file_swap && sudo fallocate -l $size /$file_swap && sudo mkswap /$file_swap && sudo swapon -p 20 /$file_swap
+if [ "$(swapon --show --noheadings | grep "$file_swap" | wc -l)" -lt 1 ]; then
+    sudo touch $file_swap && sudo chmod 600 $file_swap && sudo fallocate -l $size /$file_swap && sudo mkswap /$file_swap && sudo swapon -p 20 /$file_swap
 fi
+read input
 source build/envsetup.sh
 breakfast s3_h560
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8192M"
