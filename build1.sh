9559976b22b32e54263ec07d4e21cf49dc58997d
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-06-18 10:28:53 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -5,7 +5,6 @@ file_swap=/swapfile_$size.img
 if [ "$(swapon --show --noheadings | grep "$file_swap" | wc -l)" -lt 1 ]; then
     sudo touch $file_swap && sudo chmod 600 $file_swap && sudo fallocate -l $size /$file_swap && sudo mkswap /$file_swap && sudo swapon -p 20 /$file_swap
 fi
-read input
 source build/envsetup.sh
 breakfast s3_h560
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8192M"
