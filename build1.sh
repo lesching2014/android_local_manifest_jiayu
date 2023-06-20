968e89c2c95304c533657a15a2bff5ddd15e343d
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-06-20 18:56:39 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -1,5 +1,6 @@
 #repo forall -c git lfs pull
 #for d in ./*/ ; do (cd "$d" && git lfs pull); done
+read -p "Do you want to make installclean? [Y,n]" -i Y input
 size="10G"
 file_swap=/swapfile_$size.img
 if [ "$(swapon --show --noheadings | grep "$file_swap" | wc -l)" -lt 1 ]; then
@@ -7,8 +8,10 @@ if [ "$(swapon --show --noheadings | grep "$file_swap" | wc -l)" -lt 1 ]; then
 fi
 source build/envsetup.sh
 breakfast s3_h560
-make installclean
-export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8192M"
-export     ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8192M"
+if [[ $input == "Y" || $input == "y" || $input == "" ]]; then
+    make installclean
+fi
+#export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8192M"
+#export     ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8192M"
 export LC_ALL=C
 brunch s3_h560
