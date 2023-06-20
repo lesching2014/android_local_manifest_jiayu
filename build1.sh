87a7d2ac29faad410c8bb54b771df4b94c193e9d
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-06-20 19:03:03 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -1,6 +1,6 @@
 #repo forall -c git lfs pull
 #for d in ./*/ ; do (cd "$d" && git lfs pull); done
-read -p "Do you want to make installclean? [Y,n]" -i Y input
+read -t 10 -p "Do you want to make installclean? [Y,n]" -i Y input
 size="10G"
 file_swap=/swapfile_$size.img
 if [ "$(swapon --show --noheadings | grep "$file_swap" | wc -l)" -lt 1 ]; then
