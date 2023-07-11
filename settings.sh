448b8ec28b965a4261e2f856b202ada8b7a28981
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-07-11 10:43:52 +0800

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -5,10 +5,11 @@
 # if [[ $input == "Y" || $input == "y" || $input == "" ]]; then
     sudo apt install bc bison build-essential ccache curl \
     flex g++-multilib gcc-multilib git git-lfs gnupg gperf \
-    imagemagick lib32ncurses-dev lib32readline-dev lib32z1-dev \
+    imagemagick lib32readline-dev lib32z1-dev \
     libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev \
     libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool \
-    squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python2.7
+    squashfs-tools xsltproc zip zlib1g-dev \
+    lib32ncurses-dev lib32ncurses5-dev openjdk-8-jdk python2.7
     # sudo apt install libwxgtk3.0-dev python3
 # fi
 
