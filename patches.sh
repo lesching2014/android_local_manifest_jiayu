e1608387edf3643132078b94aa07f8a4469a4822
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-07-26 11:06:02 +0800

    Update and rename unpatches.sh to patches.sh

--- patches.sh
+++ patches.sh
@@ -8,8 +8,13 @@ echo "2. lineageos 15.1"
 echo "3. android 8.1.0"
 read -p "Choose your option:[1-3]" input
 echo ""
-read -p "Do you wnat to apply patches?[Y/n]" patches
+echo "Select patch method"
+echo "c. patch apply -v --check"
+echo "i. patch apply -v"
+echo "u. git clean -df"
+read -p "Choose your option:[C,i,u]" patches
 echo ""
+
 cd ..
 if [[ "$input" == "1" ]]; then
     branch=cm-14.1
@@ -27,11 +32,19 @@ fi
 if [ -d "$PWD/$pathname" ] ; then
     cd $pathname
 
-        if [[ "$patches" == "y" ]] || [[ "$patches" == "Y" ]] || [[ "$patches" == "" ]]; then
-            path=$(dirname "$(find ../lineage-14.1 -type f -name 'patch.sh')")
-            cd $path
-            bash patch.sh -u
-        fi
+    case $patches in
+        c|C|"") type="-c" ;; 
+        i|I) type="-i" ;; 
+        u|U) type="-u" ;;
+        *) type=""
+        echo dont know 
+        ;; 
+    esac
+    if [[ "$type" != "" ]]; then
+        path=$(dirname "$(find ../lineage-14.1 -type f -name 'patch.sh')")
+        cd $path
+        bash patch.sh $type
+    fi
 fi
 
 cd $script
